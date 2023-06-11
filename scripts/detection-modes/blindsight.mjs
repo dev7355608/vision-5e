2e54a11d647c8b067e7e07a26b655712194682aa
dev7355608 <dev7355608@gmail.com>
2023-06-11 18:46:16 +0200

    Verify compatibility with 11.301

--- scripts/detection-modes/blindsight.mjs
+++ scripts/detection-modes/blindsight.mjs
@@ -37,7 +37,8 @@ export class DetectionModeBlindsight extends DetectionMode {
 
     /** @override */
     _testLOS(visionSource, mode, target, test) {
-        if (this.angle && !this.#testAngle(visionSource, test.point)) return false;
+        if (!this._testAngle(visionSource, mode, target, test)) return false;
+        if (!this.walls) return true;
         return !CONFIG.Canvas.polygonBackends.sight.testCollision(
             { x: visionSource.x, y: visionSource.y },
             test.point,
@@ -56,31 +57,5 @@ export class DetectionModeBlindsight extends DetectionMode {
                 useThreshold: false
             }
         );
-    }
-
-    /**
-     * Test whether the point is inside the vision cone.
-     * @param {VisionSource} visionSource
-     * @param {PIXI.Point} point
-     * @returns {boolean}
-     */
-    #testAngle(visionSource, point) {
-        const { angle, rotation, externalRadius } = visionSource.data;
-
-        if (angle !== 360) {
-            const dx = point.x - visionSource.x;
-            const dy = point.y - visionSource.y;
-
-            if ((dx * dx) + (dy * dy) > externalRadius * externalRadius) {
-                const aMin = rotation + 90 - (angle / 2);
-                const a = Math.toDegrees(Math.atan2(dy, dx));
-
-                if ((((a - aMin) % 360) + 360) % 360 > angle) {
-                    return false;
-                }
-            }
-        }
-
-        return true;
     }
 }
--- scripts/detection-modes/ghostly-gaze.mjs
+++ scripts/detection-modes/ghostly-gaze.mjs
@@ -19,35 +19,4 @@ export class DetectionModeGhostlyGaze extends DetectionModeDarkvision {
             glowColor: [1, 0.4, 1, 1]
         });
     }
-
-    /** @override */
-    _testLOS(visionSource, mode, target, test) {
-        return this.#testAngle(visionSource, test.point);
-    }
-
-    /**
-     * Test whether the point is inside the vision cone.
-     * @param {VisionSource} visionSource
-     * @param {PIXI.Point} point
-     * @returns {boolean}
-     */
-    #testAngle(visionSource, point) {
-        const { angle, rotation, externalRadius } = visionSource.data;
-
-        if (angle !== 360) {
-            const dx = point.x - visionSource.x;
-            const dy = point.y - visionSource.y;
-
-            if ((dx * dx) + (dy * dy) > externalRadius * externalRadius) {
-                const aMin = rotation + 90 - (angle / 2);
-                const a = Math.toDegrees(Math.atan2(dy, dx));
-
-                if ((((a - aMin) % 360) + 360) % 360 > angle) {
-                    return false;
-                }
-            }
-        }
-
-        return true;
-    }
 }
