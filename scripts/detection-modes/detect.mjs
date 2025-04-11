8bbb7e241264abbd3b1b54ef8330f7acd460d3ba
Drag0nF <79474032+Drag0nF@users.noreply.github.com>
2025-04-11 19:55:32 +0100

    Update detect.mjs

--- scripts/detection-modes/detect.mjs
+++ scripts/detection-modes/detect.mjs
@@ -21,6 +21,9 @@ export default class DetectionModeDetect extends DetectionMode {
         if (!(target instanceof Token)
             || !target.actor
             || target.document.hasStatusEffect(CONFIG.specialStatusEffects.BURROWING)
+            || target.document.hasStatusEffect(CONFIG.specialStatusEffects.SPACE_BETWEEN_WORLDS)
+            && !source.document.hasStatusEffect(CONFIG.specialStatusEffects.SPACE_BETWEEN_WORLDS)
+            && !target.document.hasStatusEffect(CONFIG.specialStatusEffects.MATERIAL)
             || target.document.hasStatusEffect(CONFIG.specialStatusEffects.ETHEREAL)
             && !source.document.hasStatusEffect(CONFIG.specialStatusEffects.ETHEREAL)
             && !target.document.hasStatusEffect(CONFIG.specialStatusEffects.MATERIAL)
