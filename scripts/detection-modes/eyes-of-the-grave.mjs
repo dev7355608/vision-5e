5c6ec612b4b1eda5fd4d324420d1eea2d6e743e1
Drag0nF <79474032+Drag0nF@users.noreply.github.com>
2025-04-11 19:57:20 +0100

    Update eyes-of-the-grave.mjs

--- scripts/detection-modes/eyes-of-the-grave.mjs
+++ scripts/detection-modes/eyes-of-the-grave.mjs
@@ -27,6 +27,9 @@ export default class DetectionModeEyesOfTheGrave extends DetectionMode {
         if (!(target instanceof Token)
             || !target.actor?.system.details?.type
             || target.document.hasStatusEffect(CONFIG.specialStatusEffects.BURROWING)
+            || target.document.hasStatusEffect(CONFIG.specialStatusEffects.SPACE_BETWEEN_WORLDS)
+            && !source.document.hasStatusEffect(CONFIG.specialStatusEffects.SPACE_BETWEEN_WORLDS)
+            && !target.document.hasStatusEffect(CONFIG.specialStatusEffects.MATERIAL)
             || target.document.hasStatusEffect(CONFIG.specialStatusEffects.ETHEREAL)
             && !source.document.hasStatusEffect(CONFIG.specialStatusEffects.ETHEREAL)
             && !target.document.hasStatusEffect(CONFIG.specialStatusEffects.MATERIAL)
